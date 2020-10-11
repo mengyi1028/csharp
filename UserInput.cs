313d6dbf9ca409e5a5a0ac654383f4b17ea5c56e
Mengyi <39472093+mengyi1028@users.noreply.github.com>
2020-10-11 09:32:57 -0400

    Create UserInput.cs

---
+++ UserInput.cs
@@ -0,0 +1,44 @@
+using System;
+
+public class TextInput 
+{
+    public string value;
+    
+    // virtual
+    public virtual void Add(char c)
+    {
+        value +=c;
+    }
+    
+    public string GetValue()
+    {
+        return value;
+    }
+}
+
+public class NumericInput: TextInput
+{
+    
+   // override
+    public override void Add(char c)
+    {        
+        if (Char.IsDigit(c))
+        {
+            value+=c;
+        }
+    }
+    
+    
+}
+
+public class UserInput
+{
+    public static void Main(string[] args)
+    {
+        //TextInput input = new NumericInput();
+        //input.Add('1');
+        //input.Add('a');
+        //input.Add('0');
+        //Console.WriteLine(input.GetValue());
+    }
+}
